2be7c1945b92a190b018be180f8c056e497010cf
Sierra Buckland <Sierra@greatideaz.com>
2021-10-13 21:16:30 -0400

    initial commit of files

---
+++ README.md
@@ -0,0 +1,2 @@
+This is my COMP3033 Lab 2 submission. 
+By: Sierra Buckland 200366619
---
+++ routes/server.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var router = express.Router();
+ 
+router.get('/', (req,res,next) => {
+    //if there is a method query string param in url do calculation
+    if (req.query.method) {
+        var method = req.query.method;
+        var x = parseInt(req.query.x);
+        var y = parseInt(req.query.y);
+        //determine method type for calculation
+        if (method == "add") {
+          var data = {
+          x: x,
+          y: y,
+          operation: method,
+          result: x + y
+          };        
+          res.json(data).status(200); 
+        }
+        else if (method == "subtract") {
+          var data = {
+          x: x,
+          y: y,
+          operation: method,
+          result: x - y
+          };        
+          res.json(data).status(200); 
+        }
+        else if (method == "multiply") {
+          var data = {
+          x: x,
+          y: y,
+          operation: method,
+          result: x * y
+          };        
+          res.json(data).status(200); 
+        }
+        else if (method == "divide") {
+          var data = {
+          x: x,
+          y: y,
+          operation: method,
+          result: x / y
+          };        
+          res.json(data).status(200); 
+        }
+    }
+    //else if there is no method query string param in url show error message
+    else if (!req.query.lastname) {
+        console.log("Error Message: Missing method param");
+        res.json('Error Message: Missing method param').status(500);
+    }
+});
+
+module.exports = router;
